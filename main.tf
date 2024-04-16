ae2ef44c67b3a5cb83d9062d274f6b265b6335b6
PtakDev <79053918+PtakDev@users.noreply.github.com>
2024-04-16 22:18:20 +0200

    Test

--- main.tf
+++ main.tf
@@ -187,29 +187,29 @@ resource "azurerm_subnet" "int-snt" {
   address_prefixes     = var.int_subnet_config.snt_address_prefixes
 }
 
-resource "azurerm_network_security_group" "int-nsg" {
-  depends_on          = [azurerm_subnet.int-snt]
-  name                = join("", [local.vnt_name, "-nsg", var.int_subnet_config.snt_number])
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rsg.name
+# resource "azurerm_network_security_group" "int-nsg" {
+#   depends_on          = [azurerm_subnet.int-snt]
+#   name                = join("", [local.vnt_name, "-nsg", var.int_subnet_config.snt_number])
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.rsg.name
 
-  security_rule {
-    name                       = "allow_ftp_ssh"
-    priority                   = 2990
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = null
-    source_port_ranges         = [21, 22]
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-  // TAGGING
-  tags = {
-    environment = var.env_tag
-  }
-}
+#   security_rule {
+#     name                       = "allow_ftp_ssh"
+#     priority                   = 2990
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = null
+#     source_port_ranges         = [21, 22]
+#     destination_port_range     = "*"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+#   // TAGGING
+#   tags = {
+#     environment = var.env_tag
+#   }
+# }
 
 resource "azurerm_subnet_network_security_group_association" "int-nsg-association" {
   depends_on                = [azurerm_network_security_group.int-nsg]
